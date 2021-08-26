0383ae798227f8cdf70f44767a0810c7df1b67c3
jason <2B3>
2021-08-26 05:20:55 -0400

    commit 175A: then chain

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [poster, setPoster] = useState([]);
+  const inputRef = React.useRef("nothing");
+  const btnHandler = function () {
+    showResearch(inputRef.current.value);
+    inputRef.current.value = "";
+  };
+
+  function showResearch(query) {
+    fetch(`https://api.tvmaze.com/singlesearch/shows?q=${query}`)
+      .then((response) => {
+        if (!response.ok)
+          throw new Error("No shows found in the search results");
+        return response.json();
+      })
+      .then((parsedData) => {
+        console.log(parsedData);
+        setPoster((prevState) => [...prevState, parsedData.image.medium]);
+      })
+      .catch((errorObj) => {
+        alert(errorObj);
+        console.error(errorObj);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <input ref={inputRef} placeholder={"enter show here"} />
+        <button onClick={btnHandler}>Fetch Media</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {poster.length === 0 ? "" : <img src={poster[0]}></img>}
       </section>
     </React.Fragment>
   );
